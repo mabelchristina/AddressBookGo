169ff2c80bd6d3943fab0720322cf6d1bea4bb33
Mabel Christina <maybelchristina@gmail.com>
2022-12-06 11:33:08 +0530

    [Mabel] Update contact method added in addressBook

--- AddressBookGO/AddressBook/AddressBookOp.go
+++ AddressBookGO/AddressBook/AddressBookOp.go
@@ -6,7 +6,12 @@ import (
 
 var AddressArray []Contact
 
+
 func CreateContact() {
+	var length int
+	fmt.Println("Enter the name of contact add")
+	fmt.Scanln(&length)
+for a,_ := range length()
 	var firstname, lastname, email, phone string
 
 	fmt.Println("Enter First name")
@@ -26,8 +31,44 @@ func CreateContact() {
 	AddressArray = append(AddressArray, Contact)
 }
 
-func DisplayContact(){
+func DisplayContact() {
 	fmt.Printf("%v", AddressArray)
 }
 
+func UpdateContact() {
+	var name string
+	fmt.Println("Enter the name of person to edit contact")
+	fmt.Scanln(&name)
+
+	for i := range AddressArray {
+		if AddressArray[i].FirstName == name {
+			fmt.Println("Choose the field to update:")
+			var options int
+			fmt.Println("1.LastName\n2.Email\n3.Phone")
+			fmt.Scanln(&options)
+
+			switch options {
+			case 1:
+				fmt.Println("Enter Last Name :")
+				var lastName string
+				fmt.Scanln(&lastName)
+				AddressArray[i].LastName = lastName
+			case 2:
+				fmt.Println("Enter Email :")
+				var email string
+				fmt.Scanln(&email)
+				AddressArray[i].Email = email
+
+			case 3:
+				fmt.Println("Enter Phone Number:")
+				var phone string
+				fmt.Scanln(&phone)
+				AddressArray[i].Phone = phone
+
+			}
+
+		}
+		fmt.Println("No person found")
+	}
+}
 
--- AddressBookGO/main.go
+++ AddressBookGO/main.go
@@ -8,17 +8,7 @@ import (
 
 func main() {
 	fmt.Print("Welcome to addressBook in go")
-	var option int
-	fmt.Println("Enter 1 to add contact\n ")
-	fmt.Println("Enter your option:")
-	fmt.Scanln(&option)
-
-	switch option {
-
-	case 1:
-		addressbook.CreateContact()
-		addressbook.DisplayContact()
-	default:
-		fmt.Println("enter the valid option")
-	}
+	addressbook.CreateContact()
+	addressbook.DisplayContact()
+	addressbook.UpdateContact()
 }
